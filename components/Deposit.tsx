9f8fab7d14c69c94d3750f47fa1db1e92f9597a9
Christian Holman <chrisholm2@yahoo.com>
2020-12-08 09:48:00 +0100

    Updated invalid react SVG attributed:

--- components/Deposit.tsx
+++ components/Deposit.tsx
@@ -103,7 +103,7 @@ return (
         ) : (
           <div className="p-2">
             <svg className="w-4 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
             </svg>
           </div>
         )
