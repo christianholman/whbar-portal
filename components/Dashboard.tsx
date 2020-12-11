d7e407ccd7ff508feee59e6f4bca4b2c63e20768
Christian Holman <chrisholm2@yahoo.com>
2020-12-11 19:19:21 +0100

    Added introduction modal

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,8 +2,10 @@ import { useWeb3React } from "@web3-react/core";
 import React, { useEffect, useState } from "react";
 import useWHBARContract from "../hooks/useWHBARContract";
 import Deposits from "./Deposits";
+import IntroductionModal from "./IntroductionModal";
 import WHBARBalance from "./WHBARBalance";
 import WrappingStation from "./WrappingStation";
+import Modal from "react-modal";
 
 const Dashboard: React.FC = () => {
   const { account } = useWeb3React();
@@ -12,22 +14,40 @@ const Dashboard: React.FC = () => {
 
   const [ hederaAccount, setHederaAccount ] = useState("");
   const [ hasLoaded, setHasLoaded ] = useState(false);
+  const [ introductionModalShowing, setIntroductionModalShowing ] = useState(true);
 
   useEffect(() => {
-
     whbarContract._fromAccount().then(account => {
       setHederaAccount(account);
       setHasLoaded(true);
     });
   }, [])
 
+  const closeIntroductionModal = () => {
+    setIntroductionModalShowing(false);
+  }
+
   return (
     hasLoaded ? (
-      <section className="container mx-auto max-w-4xl space-y-4">
-        <WHBARBalance />
-        <WrappingStation account={account} hederaAccount={hederaAccount} />
-        <Deposits account={account} hederaAccount={hederaAccount}/>
-      </section>
+      <>
+        <Modal
+          isOpen={introductionModalShowing}
+          onRequestClose={closeIntroductionModal}
+          style={{
+            content: {
+              backgroundColor: "rgba(0,0,0,0)",
+              border: 0,
+            }
+          }}
+        >
+          <IntroductionModal onClose={closeIntroductionModal}/>
+        </Modal>
+        <section className="container mx-auto max-w-4xl space-y-4">
+          <WHBARBalance />
+          <WrappingStation account={account} hederaAccount={hederaAccount} />
+          <Deposits account={account} hederaAccount={hederaAccount}/>
+        </section>
+      </>
     ) : (
       <>
         Loading...
---
+++ components/IntroductionModal.tsx
@@ -0,0 +1,46 @@
+type IntroductionModalProps = {
+  onClose: () => void,
+}
+
+const IntroductionModal: React.FC<IntroductionModalProps> = (props) => {
+  return (
+    <div className="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
+      <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+      
+      <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded z-20 shadow-lg overflow-y-auto">
+        
+        {
+          false && (
+            <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50" onClick={props.onClose}>
+              <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+              </svg>
+              <span className="text-sm">(Esc)</span>
+            </div>
+          )
+        }
+
+        <div className="modal-content py-4 text-left px-6">
+          <div className="flex justify-between items-center pb-6">
+            <p className="text-2xl font-bold">Welcome to wHBAR!</p>
+            <div className="modal-close cursor-pointer z-50">
+              <svg onClick={props.onClose} className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+              </svg>
+            </div>
+          </div>
+            <p>
+              We are only providing a simple way of interacting with the wHBAR smart contract. If you send HBAR to the wrong Hedera account or use the wrong memo (your ethereum address) you will lose your HBAR.
+              We take <span className="font-bold">zero </span>responsibility for any funds lost.
+            </p>
+          <div className="flex justify-end pt-6">
+            <button className="px-4 bg-blue-500 p-3 rounded text-white hover:bg-blue-400 hover:cursor-pointer" onClick={props.onClose}>Understand</button>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroductionModal;
