de11ad6f2922310cb7221eac4029384ccbc5aeb7
Christian Holman <chrisholm2@yahoo.com>
2020-12-12 20:55:11 +0100

    Added social media meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
         <meta name="description" content="Use the wHBAR portal to mint wHBAR on ethereum." />
         <base target="_blank"/>
+        <meta name="og:title" content="wHBAR Portal" />
+        <meta name="og:description" content="Use the wHBAR portal to mint wHBAR on ethereum." />
+        <meta name="og:image" content="https://d1pnnwteuly8z3.cloudfront.net/images/6257c2da-dd7b-486d-a4ec-130b8125da8b/d809bb7d-238f-4879-94b4-65573c9477e3.png" />
+        <meta name="og:url" content="https://portal.whbar.co" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:site_name" content="wHBAR Portal" />
+        <meta name="twitter:image:alt" content="wHBAR Logo" />
       </Head>
 
       <main>
