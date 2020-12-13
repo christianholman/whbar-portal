cec520f45c511aa7e228e19fdfe767e20ae0cd55
Christian Holman <chrisholm2@yahoo.com>
2020-12-13 18:23:42 +0100

    Updated IntroductionModal padding

--- components/IntroductionModal.tsx
+++ components/IntroductionModal.tsx
@@ -20,14 +20,9 @@ const IntroductionModal: React.FC<IntroductionModalProps> = (props) => {
           )
         }
 
-        <div className="modal-content py-4 text-left px-6">
+        <div className="modal-content p-6 text-left">
           <div className="flex justify-between items-center pb-6">
             <p className="text-2xl font-bold">Welcome to wHBAR!</p>
-            <div className="modal-close cursor-pointer z-50">
-              <svg onClick={props.onClose} className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-              </svg>
-            </div>
           </div>
             <p>
               We are only providing a simple way of interacting with the wHBAR smart contract. If you send HBAR to the wrong Hedera account or use the wrong memo (your ethereum address) you will lose your HBAR.
